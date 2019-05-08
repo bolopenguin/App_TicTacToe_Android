62cb8c412b931350852d068f0e1e4ed1608674e6
Damiano Bolognini <damianobolognini1997@gmail.com>
2019-05-08 14:42:56 +0200

    sistemato icone dialog

--- app/src/main/java/com/example/progettoandroid/MainActivity.java
+++ app/src/main/java/com/example/progettoandroid/MainActivity.java
@@ -708,7 +708,7 @@ public class MainActivity extends AppCompatActivity
     public void Info (View view) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
         alertDialogBuilder.setTitle("Istruzioni");
-        alertDialogBuilder.setIcon(R.drawable.ic_stat_name);
+        alertDialogBuilder.setIcon(R.drawable.bluetoothconnection);
         alertDialogBuilder.setMessage("1)Decidere chi tra te e i tuo avversario inizia a giocare," +
 
                 "\n2)Colui che inizia sarà giocatore 1 e l'altro giocatore 2," +
